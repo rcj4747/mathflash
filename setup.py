abcfe2125fd522c8c953efb1177bd5781e106bfa
Robert C Jennings <robert.jennings@canonical.com>
2020-07-03 17:10:47 -0500

    Initial commit of the math flashcard game

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mathflash',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Robert C Jennings',
+    author_email='rcj4747@gmail.com',
+    description='A Math Flash Cards Game'
+)
