6827f58c027385237d0758f6bc244634c92b39de
Robert C Jennings <robert.jennings@canonical.com>
2020-07-04 10:51:31 -0500

    Fix setup and move code to allow easy execution

--- setup.py
+++ setup.py
@@ -1,12 +1,32 @@
-from setuptools import setup
+"""
+A simple math flashcard program
+"""
+import os
+
+from setuptools import find_packages, setup
+
+reqs_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+
+with open(reqs_path, 'r') as req_file:
+    dependencies = req_file.readlines()
 
 setup(
     name='mathflash',
-    version='',
-    packages=[''],
+    version='0.1.0',
+    packages=find_packages(),
     url='',
-    license='',
+    license='GPLv3',
     author='Robert C Jennings',
     author_email='rcj4747@gmail.com',
-    description='A Math Flash Cards Game'
+    description='A Math Flash Cards Game',
+    long_description=__doc__,
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'mathflash = mathflash:main',
+        ],
+    },
 )
